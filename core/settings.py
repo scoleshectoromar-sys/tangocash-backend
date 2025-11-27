e7c8e56600a60d3311fdaa3e24f271cc07f35f69
Hosel777 <businesslatamarg@gmail.com>
2025-11-27 05:47:43 -0300

    fix: final settings for render deployment

--- core/settings.py
+++ core/settings.py
@@ -133,3 +133,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Fin de configuraciones
